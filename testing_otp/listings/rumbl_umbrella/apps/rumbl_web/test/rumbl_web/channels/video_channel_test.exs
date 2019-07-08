bcace929921c7a6170283798d02db8a30af6a4d7
Arafat M <github@arafatm.com>
2019-07-07 19:16:51 -0700

    init

---
+++ testing_otp/listings/rumbl_umbrella/apps/rumbl_web/test/rumbl_web/channels/video_channel_test.exs
@@ -0,0 +1,28 @@
+#---
+# Excerpted from "Programming Phoenix 1.4",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/phoenix14 for more book information.
+#---
+defmodule RumblWeb.Channels.VideoChannelTest do
+  use RumblWeb.ChannelCase
+  import RumblWeb.TestHelpers
+
+  setup do 
+    user = insert_user(name: "Gary") 
+    video = insert_video(user, title: "Testing")
+    token = Phoenix.Token.sign(@endpoint, "user socket", user.id)
+    {:ok, socket} = connect(RumblWeb.UserSocket, %{"token" => token})
+
+    {:ok, socket: socket, user: user, video: video}
+  end
+
+  test "inserting new annotations", %{socket: socket, video: vid} do
+    {:ok, _, socket} = subscribe_and_join(socket, "videos:#{vid.id}", %{})
+    ref = push socket, "new_annotation", %{body: "the body", at: 0}
+    assert_reply ref, :ok, %{}
+    assert_broadcast "new_annotation", %{}
+  end
+end
